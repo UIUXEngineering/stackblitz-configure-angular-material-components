1f22248cef56bc3b27aa601161c0d3e88182a568
jorta <jorta@octolabs.io>
2020-06-04 11:46:57 -0500

    feat(typography): add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './pages/home/home.component';
+import { TypographyComponent } from './pages/typography/typography.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TypographyComponent
   ],
   imports: [
     BrowserModule,
